1433cdc50f592b666749a7cca2a6b19569c2e08b
vivzi <vivaanninja@outlook.com>
2024-09-15 20:32:27 +0530

    a

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-import { firebaseConfig } from "config.js"
+const firebaseConfig = {
+    apiKey: FIREBASE_API_KEY,
+    authDomain: "bytehustle-1.firebaseapp.com",
+    databaseURL: "https://bytehustle-1-default-rtdb.firebaseio.com",
+    projectId: "bytehustle-1",
+    storageBucket: "bytehustle-1.appspot.com",
+    messagingSenderId: "432556295891",
+    appId: "1:432556295891:web:d13c2c998cddef6a817626",
+    measurementId: "G-3F3M1TC2JF",
+    //databaseURL: "bytehustle-1-default-rtdb.firebaseio.com"
+};
 
 // Initialize Firebase
 const firebaseApp = firebase.initializeApp(firebaseConfig);
